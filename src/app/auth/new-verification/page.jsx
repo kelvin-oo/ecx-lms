f76e7a1aa728ffead1cd6ab2f0a824e2140b22fd
kelvin-oo <olamilekan.obisesan1@gmail.com>
2024-05-14 21:26:27 +0100

    depoyment error fix 3.

--- src/app/auth/new-verification/page.jsx
+++ src/app/auth/new-verification/page.jsx
@@ -3,14 +3,13 @@
 import { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import Image from "next/image";
-import authStyles from "@/components/css/authLayout.module.css"
+import authStyles from "@/components/css/authLayout.module.css";
 import Link from "next/link";
 import { newVerification } from "@/actions/newVerification";
 import ComponentLevelLoader from "@/components/Loader";
 
-export default function NewVerification() {
-
-    const [error, setError] = useState();
+export default function page() {
+  const [error, setError] = useState();
   const [success, setSuccess] = useState();
 
   const searchParams = useSearchParams();
@@ -32,7 +31,7 @@ export default function NewVerification() {
       })
       .catch(() => {
         setError("Something went wrong!");
-      })
+      });
   }, [token, success, error]);
 
   useEffect(() => {
@@ -60,27 +59,20 @@ export default function NewVerification() {
           height={64}
         />
 
-        <div className="flex flex-col items-center gap-8 mt-24 lg:mt-32"> 
-        {!success && !error && <h1 className={authStyles.auth__title}>
-            Confirming Verification...
-          </h1>}
-          {
-            success && <h1 className={authStyles.auth__title}>
-            {success}
-          </h1>
-          }
-          {
-            error && <h1 className={authStyles.auth__title}>
-            {error}
-          </h1>
-          }
+        <div className="flex flex-col items-center gap-8 mt-24 lg:mt-32">
           {!success && !error && (
-          <ComponentLevelLoader />
-        )}
-          <Link href="/auth/login" className='border-[3px] border-ecx-colors-secondary-blue text-ecx-colors-secondary-blue p-2'>
-          <h6>
-            Go to Login
-          </h6>
+            <h1 className={authStyles.auth__title}>
+              Confirming Verification...
+            </h1>
+          )}
+          {success && <h1 className={authStyles.auth__title}>{success}</h1>}
+          {error && <h1 className={authStyles.auth__title}>{error}</h1>}
+          {!success && !error && <ComponentLevelLoader />}
+          <Link
+            href="/auth/login"
+            className="border-[3px] border-ecx-colors-secondary-blue text-ecx-colors-secondary-blue p-2"
+          >
+            <h6>Go to Login</h6>
           </Link>
         </div>
 
@@ -93,5 +85,5 @@ export default function NewVerification() {
         />
       </main>
     </>
-  )
+  );
 }
