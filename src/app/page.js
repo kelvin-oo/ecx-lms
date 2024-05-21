6811d496e79b144b395476d2d5cfd53bb7b8916d
SixtusOnyedibe <sixtusonyedibe@gmail.com>
2024-05-21 15:58:16 +0100

    Upadated the suth routes and signin routes

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
-import Link from "next/link";
-import AuthLayout from "@/components/auth/AuthLayout";
+import Link from 'next/link';
+import AuthLayout from '@/components/auth/AuthLayout';
 
 export default function Home() {
   return (
     <AuthLayout>
-      <div className="grid grid-cols-2 px-4 w-full max-w-[400px] lg:max-w-[550px] mx-auto gap-6 lg:gap-16 text-sm lg:text-lg font-varela-round [&>*]:col-span-1 [&>*]:flex [&>*]:items-center [&>*]:justify-center [&>*]:h-10 [&>*]:lg:h-14">
+      <div className='grid grid-cols-2 px-4 w-full max-w-[400px] lg:max-w-[550px] mx-auto gap-6 lg:gap-16 text-sm lg:text-lg font-varela-round [&>*]:col-span-1 [&>*]:flex [&>*]:items-center [&>*]:justify-center [&>*]:h-10 [&>*]:lg:h-14 '>
         <Link
-          href="/auth/participant/signup"
-          className="bg-ecx-colors-secondary-blue text-white"
+          href='/auth/signup'
+          className='bg-ecx-colors-secondary-blue text-white'
         >
           Sign up
         </Link>
         <Link
-          href="/auth/participant/login"
-          className="border-[3px] border-ecx-colors-secondary-blue text-ecx-colors-secondary-blue"
+          href='/auth/login'
+          className='border-[3px] border-ecx-colors-secondary-blue text-ecx-colors-secondary-blue'
         >
           Login
         </Link>
