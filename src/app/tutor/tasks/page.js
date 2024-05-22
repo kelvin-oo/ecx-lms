25949616f0df44e4ef32433dd7127d3b78de3d13
SixtusOnyedibe <sixtusonyedibe@gmail.com>
2024-05-22 21:05:06 +0100

    Added link to the add task page

--- src/app/tutor/tasks/page.js
+++ src/app/tutor/tasks/page.js
@@ -1,37 +1,44 @@
-"use client"
-import AdminTasksTable from "@/components/admin/AdminTasksTable";
-import SubmissionsTable from "@/components/admin/SubmissionsTable";
-import tasks from "@/sampleData/adminTasks.json"
+'use client';
+import AdminTasksTable from '@/components/admin/AdminTasksTable';
+import SubmissionsTable from '@/components/admin/SubmissionsTable';
+import tasks from '@/sampleData/adminTasks.json';
 import Image from 'next/image';
-import { useState } from "react";
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function AdminTasksPage() {
-  const [isShowSubmissions, setShowSubmissions] = useState(false)
+  const [isShowSubmissions, setShowSubmissions] = useState(false);
 
-  const toggleShowSubmissions = () => setShowSubmissions(!isShowSubmissions)
+  const toggleShowSubmissions = () => setShowSubmissions(!isShowSubmissions);
 
   return (
     <div>
-      <div className="flex items-center justify-between lg:justify-end">
-        <p className="lg:hidden mt-2"><b>Track: </b>Front end Web Development</p>
-        
-        <button className="bg-ecx-colors-secondary-blue text-white font-semibold text-xs lg:text-base py-2.5 lg:py-3 px-3.5 lg:px-5 hover:opacity-90 transition-opacity">
-          Task Management
-        </button>
+      <div className='flex items-center justify-between lg:justify-end'>
+        <p className='lg:hidden mt-2'>
+          <b>Track: </b>Front end Web Development
+        </p>
+
+        <Link href='/tutor/add-newtask'>
+          <button className='bg-ecx-colors-secondary-blue text-white font-semibold text-xs lg:text-base py-2.5 lg:py-3 px-3.5 lg:px-5 hover:opacity-90 transition-opacity'>
+            Add New Task
+          </button>
+        </Link>
       </div>
 
-      <div className="mt-5 flex flex-col gap-10 xl:gap-x-8 xl:gap-y-7">
+      <div className='mt-5 flex flex-col gap-10 xl:gap-x-8 xl:gap-y-7'>
         <AdminTasksTable tasksArr={tasks} />
 
         <div
           onClick={toggleShowSubmissions}
-          className="flex items-center justify-between py-3 border-b border-black select-none cursor-pointer"
+          className='flex items-center justify-between py-3 border-b border-black select-none cursor-pointer'
         >
           <span>Show Task Submissions</span>
           <Image
-            src="/icons/chevron-down-black.svg"
-            className={`transition-transform ${isShowSubmissions && "rotate-180"}`}
-            alt="arrow"
+            src='/icons/chevron-down-black.svg'
+            className={`transition-transform ${
+              isShowSubmissions && 'rotate-180'
+            }`}
+            alt='arrow'
             width={20}
             height={20}
           />
@@ -41,4 +48,4 @@ export default function AdminTasksPage() {
       </div>
     </div>
   );
-};
+}
