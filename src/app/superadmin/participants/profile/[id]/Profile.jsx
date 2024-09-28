c827b8c662ded4631b84d6ee48f1e8208f237914
kelvin-oo <olamilekan.obisesan1@gmail.com>
2024-09-28 21:33:33 +0100

    Feat: User block and instantaneous update.

--- src/app/superadmin/participants/profile/[id]/Profile.jsx
+++ src/app/superadmin/participants/profile/[id]/Profile.jsx
@@ -1,11 +1,20 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
+import { useQueryClient, useQuery } from "@tanstack/react-query";
 import FormBox from "@/components/profile/FormBox";
 import FormBoxHeader from "@/components/profile/FormBoxHeader";
 import { getUserDetailsAndTaskInfo } from "@/actions/superAdmin/super";
 import Image from "next/image";
+import style from "../../../../../styles/popup.module.scss";
+import styles from "@/components/css/sidebar.module.css";
+import { useState } from "react";
+import ComponentLevelLoader from "@/components/Loader";
+import { userStatusChange } from "@/actions/superAdmin/super";
+import { toast } from "react-toastify";
 
 const Profile = ({ id }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const queryClient = useQueryClient();
   const { data, error, isLoading, isFetched } = useQuery({
     queryKey: ["profile"],
     queryFn: async () => {
@@ -15,7 +24,41 @@ const Profile = ({ id }) => {
       }
       return result.success;
     },
+    refetchOnMount: true
   });
+  const handleUserBlock = async (id, status) => {
+    console.log("🚀 ~ handleUserBlock ~ status:", status);
+    console.log("🚀 ~ handleUserBlock ~ id:", id);
+    setLoading(true);
+
+    const result = await userStatusChange(id, status);
+    console.log("🚀 ~ handleUserBlock ~ result:", result);
+    if (result.success) {
+      queryClient.invalidateQueries('profile');
+      setOpenModal(false);
+      setLoading(false);
+      toast.success(result.success, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      
+    } else if (result.error) {
+      setOpenModal(false);
+      setLoading(false);
+      toast.error(result.error, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+    }
+  };
   console.log("🚀 ~ Profile ~ data:", data);
   return (
     <section>
@@ -25,9 +68,44 @@ const Profile = ({ id }) => {
           data.user.image && <Image src={data?.user.image} alt="" width={500} height={500 } className="rounded-full w-[100%] h-[100%]"/>
         }
       </div>
-        <button className='bg-ecx-colors-secondary-blue text-white font-semibold text-xs lg:text-base py-2.5 lg:py-3 px-3.5 lg:px-5 hover:opacity-90 transition-opacity absolute right-0 top-0'>
-          Block admin
+        <button onClick={() => setOpenModal(!openModal)} className='bg-ecx-colors-secondary-blue text-white font-semibold text-xs lg:text-base py-2.5 lg:py-3 px-3.5 lg:px-5 hover:opacity-90 transition-opacity absolute right-0 top-0 lg:w-[10rem]'>
+        {
+          data.user.status === 'ACTIVE' ? 'Block  User' : 'Unblock User'
+         }
         </button>
+        {openModal && (
+          <div className={style.popup}>
+            <div className={style.overlay}></div>
+            <div className={style.modalContent}>
+              <div className="flex flex-col gap-2 bg-white px-16 py-6">
+                <div className="text-center">ARE YOU SURE?</div>
+                <div className="flex gap-5">
+                  {/* <LogoutButton> */}
+                  <button
+                    onClick={() =>
+                      handleUserBlock(data.user.id, data.user.status)
+                    }
+                    className={`${styles.modalBtn} ${styles.modalBtnDark} p-2`}
+                  >
+                    {loading ? (
+                      <ComponentLevelLoader color={"#ffffff"} />
+                    ) : (
+                      "YES"
+                    )}
+                  </button>
+                  {/* </LogoutButton> */}
+
+                  <button
+                    onClick={() => setOpenModal(false)}
+                    className={`${styles.modalBtn} p-2`}
+                  >
+                    NO
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
       <form className="grid grid-cols-1 gap-y-10 pb-8 ">
         <FormBox>
